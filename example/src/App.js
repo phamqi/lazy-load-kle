e75e4f6860505c9087b79f5b0398507182396178
phamqi <phamquy2425@gmail.com>
2022-07-03 17:03:13 +0700

    init create-react-library@3.1.1

---
+++ example/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'lazy-load-kle'
+import 'lazy-load-kle/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
