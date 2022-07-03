01f69edac9d5fbded2719c8fc5c05b111f7f6139
phamqi <phamquy2425@gmail.com>
2022-07-03 18:20:47 +0700

    Updates

---
+++ precache-manifest.52710f5e13048a8a4b8726654babd9a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "034a7233237adc96199cda0d0748be8c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e0f74798dc941ca10444",
+    "url": "./static/css/main.024dfb53.chunk.css"
+  },
+  {
+    "revision": "234a022c235538d6d58e",
+    "url": "./static/js/2.a673508d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.a673508d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0f74798dc941ca10444",
+    "url": "./static/js/main.455b2209.chunk.js"
+  },
+  {
+    "revision": "17bf039fa77b65bbcea0",
+    "url": "./static/js/runtime-main.f157bd6b.js"
+  }
+]);
